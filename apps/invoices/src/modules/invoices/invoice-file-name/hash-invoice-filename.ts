f10b9f956aae4873c437799a0fe3d4fc7222bb3e
Tha-Toe <thatoe510@gmail.com>
2026-08-06 11:33:23 +0700

    removed UUID from invoice

--- apps/invoices/src/modules/invoices/invoice-file-name/hash-invoice-filename.ts
+++ apps/invoices/src/modules/invoices/invoice-file-name/hash-invoice-filename.ts
@@ -1,5 +1,6 @@
-import { randomUUID } from "crypto";
+// import { randomUUID } from "crypto";
 
 export const hashInvoiceFilename = (invoiceName: string, orderId: string) => {
-  return `${invoiceName}_${orderId}_${randomUUID()}`;
+  // return `${invoiceName}_${orderId}_${randomUUID()}`;
+  return `${invoiceName}_${orderId}`;
 };
